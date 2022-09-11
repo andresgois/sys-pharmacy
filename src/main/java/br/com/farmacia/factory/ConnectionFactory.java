e8bf15520e1daf93a41cfa2438e89c5632090fea
Andre Gois <andresousalee@gmail.com>
2022-09-11 15:12:16 -0300

    :bug:fix: correcao do nome da classe

---
+++ src/main/java/br/com/farmacia/factory/ConnectionFactory.java
@@ -0,0 +1,28 @@
+package br.com.farmacia.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    
+    private static final String USUARIO = "andre";
+    private static final String SENHA = "123456";
+    private static final String URL = "jdbc:mysql://localhost:3306/farmacia?autoReconnect=true&useSSL=false";
+    
+    public static Connection conectar() throws SQLException {
+        Connection conexao = (Connection) DriverManager.getConnection(URL,
+                USUARIO, SENHA);
+        return conexao;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            Connection conexao = ConnectionFactory.conectar();
+            System.out.println("Conectado com sucesso!!");
+        } catch (Exception e) {
+            System.out.println("Falha na Coneção");
+        }
+    }
+    
+}
